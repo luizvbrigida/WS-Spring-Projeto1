ee21666f715a775f1891aad29fb428937aa5ca3f
Luiz Vinicius Brígida <l.vinicius_@hotmail.com>
2021-10-28 00:16:51 -0300

    St project

---
+++ src/main/java/com/example/demo/curso/CursoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	
+	// comentario
+}
